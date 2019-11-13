35a7ef6003af964fff35fc9938e9939515d7c51b
mphelps1978 <phelps.michael@live.com>
2019-11-13 16:35:26 -0600

    20 Passing

--- index.js
+++ index.js
@@ -72,7 +72,17 @@ Person.prototype.toString = function() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {}
+function Car(model, milesPerGallon) {
+	(this.model = model), (this.milesPerGallon = milesPerGallon), (this.tank = 0), (this.odometer = 0);
+}
+
+Car.prototype.fill = function(gallons) {
+	this.tank += gallons;
+};
+
+Car.prototype.drive = function(distance) {
+	this.odometer += distance;
+};
 
 /*
   TASK 3
@@ -81,7 +91,15 @@ function Car() {}
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {}
+function Baby(favoriteToy) {
+	Person.call(this, favoriteToy);
+	this.isChild = favoriteToy.isChild;
+}
+Baby.prototype = Object.create(Person.prototype);
+
+Baby.prototype.play = function() {
+	return `Playing with ${this.favoriteToy}`;
+};
 
 /*
   TASK 4
